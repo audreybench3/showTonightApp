74e7ef50fb9369abfac7d707508f5009e8a5aa37
audreybench3 <audrey.benchaya@gmail.com>
2016-10-17 22:27:12 +0200

    manque le modal

--- www/js/services.js
+++ www/js/services.js
@@ -1,44 +1,18 @@
 angular.module('starter.services', [])
 
-.factory('Shows', function() {
+.factory('Shows', function($http) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var shows = [{
-    id: 1,
-    name: 'Audrey',
-    location: 'Chez Audrey',
-    description: 'best show',
-    capacity: 100,
-    price: 100,
-    image: "http://www.woueb.net/wp-content/uploads/2010/12/salle-de-concert-360.jpg",
-    date: '2016-09-28',
-    url: "https://api-shows-tonight.herokuapp.com/shows/1.json"
-  }, {
-    id: 2,
-    name: 'Beyonce',
-    location: 'Chez Audrey',
-    description: 'best show ever',
-    capacity: 100,
-    price: 1000,
-    image: "http://www.woueb.net/wp-content/uploads/2010/12/salle-de-concert-360.jpg",
-    date:'2016-09-28',
-    url:"https://api-shows-tonight.herokuapp.com/shows/2.json"
-  }, {
-    id: 3,
-    name: 'Showtime',
-    location: 'Stade de France',
-    description: 'show',
-    capacity: 200,
-    price: 200,
-    image: "http://www.woueb.net/wp-content/uploads/2010/12/salle-de-concert-360.jpg",
-    date: '2016-09-28',
-    url:"https://api-shows-tonight.herokuapp.com/shows/5.json"
-  },
-  ];
+  var shows = []
+    
   return {
     all: function() {
-      return shows;
+      return $http.get("https://api-audreys7.herokuapp.com/shows.json")
+        .then(function(response) {
+          shows = response.data;
+          return shows;
+        })
     },
 
     get: function(showId) {
@@ -48,6 +22,14 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+
+    book: function(showId, user_name, seats) {
+      return $http.post("https://api-audreys7.herokuapp.com/shows/" + showId + "/book.json", {booking: {user_name: user_name, seats: seats}}).then(function(response){
+        booking = response.data;
+        return booking;
+      });
     }
+
   };
 });
